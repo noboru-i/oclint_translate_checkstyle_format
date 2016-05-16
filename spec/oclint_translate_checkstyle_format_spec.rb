7301f3308bfe3a229880944d87d19833b985e251
noboru-i <ishikura.noboru@gmail.com>
2016-05-16 17:42:44 +0900

    temp delete test

--- spec/oclint_translate_checkstyle_format_spec.rb
+++ spec/oclint_translate_checkstyle_format_spec.rb
@@ -4,8 +4,4 @@ describe OclintTranslateCheckstyleFormat do
   it 'has a version number' do
     expect(OclintTranslateCheckstyleFormat::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
